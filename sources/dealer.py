c0fbc1d6c027c1322ce02a72dc1001e1ba627870
Samael <jeremie.joannard@epitech.eu>
2026-03-25 23:07:33 +0100

    basic_sys
    [ADD] Makefile, [ADD] sources/,

---
+++ sources/dealer.py
@@ -0,0 +1,9 @@
+##
+## EPITECH PROJECT, 2022
+## Untitled (Workspace)
+## File description:
+## dealer
+##
+
+def dealer():
+    print("Hello World")
---
+++ sources/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+##
+## EPITECH PROJECT, 2022
+## Untitled (Workspace)
+## File description:
+## main
+##
+
+import sources.dealer as dealer
+
+if __name__ == '__main__':
+    dealer.dealer()
